c783b1aad6b56f6ff4883dca20c9bf52e419be3f
Charles Marques <charles_sjp@yahoo.com.br>
2023-04-22 09:06:55 -0300

    change main

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true
   });
 
-
+  app.set('trust proxy', 1)
 
   app.useGlobalPipes(new ValidationPipe());
 
